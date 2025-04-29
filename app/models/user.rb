951d38eebb6e16e57a4cea3e21d4be44bac8c07a
Chandhu Bhumireddy <89249579+bchandhu@users.noreply.github.com>
2025-04-29 21:35:17 +0000

    Add full_name method and sample seed data

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,8 @@
 #  id                     :bigint           not null, primary key
 #  email                  :string           default(""), not null
 #  encrypted_password     :string           default(""), not null
+#  first_name             :string
+#  last_name              :string
 #  name                   :string
 #  remember_created_at    :datetime
 #  reset_password_sent_at :datetime
@@ -24,5 +26,10 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :diagnostic_responses, class_name: "Diagnostic", foreign_key: "user_id", dependent: :destroy
   has_many :calendar_entries, class_name: "Log", foreign_key: "user_id", dependent: :destroy
+  
+  def full_name
+    "#{first_name} #{last_name}".strip
+  end
+  
          
 end
