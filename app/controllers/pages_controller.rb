7dfecf34e0d415e215965ac27ae786559723529b
Chandhu Bhumireddy <89249579+bchandhu@users.noreply.github.com>
2025-04-30 05:13:37 +0000

    worked on dashboard cards

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,11 +6,14 @@ class PagesController < ApplicationController
   end
 
   def dashboard
-  @latest_diagnostic = current_user.diagnostic_responses.order(created_at: :desc).first
-  @recent_logs = current_user.calendar_entries.order(date: :desc).limit(5)
+    @latest_diagnostic = current_user.diagnostic_responses.order(created_at: :desc).first
 
-  @this_month_logs = current_user.calendar_entries.where(date: Time.zone.today.beginning_of_month..Time.zone.today.end_of_month)
-  @last_log = current_user.calendar_entries.order(date: :desc).first
-end
+    @recent_logs = current_user.calendar_entries.order(date: :desc).limit(3)
 
+    @this_month_logs = current_user.calendar_entries.where(
+      date: Time.zone.today.beginning_of_month..Time.zone.today.end_of_month
+    )
+
+    @last_log = current_user.calendar_entries.order(date: :desc).first
+  end
 end
